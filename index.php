6277afa104624ed08858a0acd7707db71c422db8
Fanny Surjana <fsurjana@mw2consulting.com>
2014-07-14 12:00:17 -0700

    Main cmd line

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
+	switch( $argv[1] ){
+		case "-lineno":
+			if( $argv[2] ){
+				$CURRENT = $argv[2];
+			} else {
+				echo "Incomplete param.  Do php index.php -lineno <line nomer dari KELURAHAN_ID.csv>\n";
+				exit();
+			}
+			break;
+		default:
+			echo "Unknown param\n";
+			exit();
+	}
+
+	$file = new SplFileObject('KELURAHAN_ID.csv');
+	try{
+		while(TRUE) {
+			$file->seek($CURRENT-1);
+			$kelurahan_id = $file->current();
+			$child = popen('php child.php -id '.$kelurahan_id, 'r');
+			$response = stream_get_contents($child);
+			echo $response;
+			if(preg_match("/Incomplete param/", $response)) return FALSE;
+			$CURRENT++;
+		}
+	} catch (Exception $e) {
+		throw $e;
+		return -1;
+	}
+
 
 
 ?>
-Halo dunia!
